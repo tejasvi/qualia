9bf6aa4ac910fef1c8638f5236c326feae7bcc34
tejasvi <45873379+tejasvi@users.noreply.github.com>
2021-08-26 22:40:07 +0530

    Ensure pip exists

--- rplugin/python3/qualia/utils/requirements_utils.py
+++ rplugin/python3/qualia/utils/requirements_utils.py
@@ -4,9 +4,10 @@ from sys import executable
 
 
 def install_qualia_dependencies() -> None:
+    check_call([executable, " -m", "ensurepip", "--default-pip"])
     requirements_file_path = Path(__file__).parent.parent.parent.joinpath("requirements.txt").as_posix()
     install_command = [executable, "-m", "pip", "install", "-r", requirements_file_path]
     try:
-        check_call(install_command)
-    except CalledProcessError:
         check_call(install_command + ["--user"])
+    except CalledProcessError:
+        check_call(install_command)
