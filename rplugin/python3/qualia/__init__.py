eb1b2e5fd1d02ee19f727203a7a56fca86b0c289
tejasvi <45873379+tejasvi@users.noreply.github.com>
2021-08-27 07:21:22 +0530

    Support different pip directory

--- rplugin/python3/qualia/__init__.py
+++ rplugin/python3/qualia/__init__.py
@@ -1,7 +1,12 @@
+from pathlib import Path
+from sys import path
+
+optional_install_dir = Path().home().joinpath('.qualia_packages').as_posix()
+path.insert(0, optional_install_dir)
 try:
     from qualia.commands import Qualia
 except ModuleNotFoundError:
     from qualia.utils.requirements_utils import install_qualia_dependencies
 
-    install_qualia_dependencies()
+    install_qualia_dependencies(optional_install_dir)
     from qualia.commands import Qualia
--- rplugin/python3/qualia/utils/requirements_utils.py
+++ rplugin/python3/qualia/utils/requirements_utils.py
@@ -1,13 +1,17 @@
+from os import environ
 from pathlib import Path
 from subprocess import check_call, CalledProcessError
 from sys import executable
 
 
-def install_qualia_dependencies() -> None:
+def install_qualia_dependencies(optional_install_dir: str) -> None:
     check_call([executable, " -m", "ensurepip", "--default-pip"])
     requirements_file_path = Path(__file__).parent.parent.parent.joinpath("requirements.txt").as_posix()
     install_command = [executable, "-m", "pip", "install", "-r", requirements_file_path]
     try:
         check_call(install_command + ["--user"])
     except CalledProcessError:
-        check_call(install_command)
+        try:
+            check_call(install_command)
+        except CalledProcessError:
+            check_call(install_command, env=dict(environ, PIP_TARGET=optional_install_dir))
