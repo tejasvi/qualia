f99e91440199ab5b225238ad59e323a4aeae5379
tejasvi <45873379+tejasvi@users.noreply.github.com>
2021-08-27 11:27:03 +0530

    Error reporting

--- rplugin/python3/qualia/__init__.py
+++ rplugin/python3/qualia/__init__.py
@@ -5,7 +5,7 @@ optional_install_dir = Path().home().joinpath('.qualia_packages').as_posix()
 path.insert(0, optional_install_dir)
 try:
     from qualia.commands import Qualia
-except ModuleNotFoundError:
+except (ModuleNotFoundError, ImportError):
     from qualia.utils.requirements_utils import install_qualia_dependencies
 
     install_qualia_dependencies(optional_install_dir)
