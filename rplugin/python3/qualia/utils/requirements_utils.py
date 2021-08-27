49f70987abf33d7351e09fc991d373c2489cdd9c
tejasvi <45873379+tejasvi@users.noreply.github.com>
2021-08-27 08:47:17 +0530

    Optionally install with system python

--- rplugin/python3/qualia/utils/requirements_utils.py
+++ rplugin/python3/qualia/utils/requirements_utils.py
@@ -15,18 +15,24 @@ def install_qualia_dependencies(optional_install_dir: str) -> None:
         pass
 
     requirements_file_path = Path(__file__).parent.parent.parent.joinpath("requirements.txt").as_posix()
+
+    init_command = [executable, "-m", "pip", "install", "setuptools", "wheel"]
     install_command = [executable, "-m", "pip", "install", "-r", requirements_file_path]
+
     try:
-        check_call(install_command + ["--user"])
+        for command in (init_command, install_command):
+            check_call(command + ["--user"] )
     except CalledProcessError:
         try:
-            check_call(install_command)
+            for command in (init_command, install_command):
+                check_call(command)
         except CalledProcessError:
             default_python = [get_location("py"), "-3"] if name == 'nt' else [get_location("python3")]
             try:
                 check_call(default_python + ["-m", "ensurepip", "--default-pip"])
             except CalledProcessError:
                 pass
-            check_call(default_python + install_command[1:],
-                       env=dict(environ, PIP_TARGET=optional_install_dir,
-                                PYTHONPATH=environ.get("PYTHONPATH", "") + pathsep + optional_install_dir))
+            check_call(default_python + init_command[1:])
+            check_call(default_python + install_command[1:], env=dict(environ, PIP_TARGET=optional_install_dir,
+                                                                      PYTHONPATH=(environ["PYTHONPATH"] + pathsep +
+                                                                                  optional_install_dir) if "PYTHONPATH" in environ else optional_install_dir))
