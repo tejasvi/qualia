d3268e085fc3415ed08e1039ab7d7c6fa7ca1a63
tejasvi <45873379+tejasvi@users.noreply.github.com>
2021-10-12 19:10:22 +0530

    Comment out pydev

--- rplugin/python3/qualia/utils/perf_utils.py
+++ rplugin/python3/qualia/utils/perf_utils.py
@@ -5,7 +5,7 @@ from time import time
 def perf_imports() -> None:
     import builtins
 
-    original_import = builtins.__import__  # type: ignore[misc]
+    original_import = builtins.__import__
     # timer = perf_counter
     timer = time
     last_time = start_time = timer()
@@ -13,13 +13,13 @@ def perf_imports() -> None:
     def custom_import(name: str, *args: str, **kwargs: object) -> object:
         nonlocal last_time
         import_start_time = timer()
-        result = original_import(name, *args, **kwargs)  # type: ignore[misc,arg-type]
+        result = original_import(name, *args, **kwargs)  # type: ignore
         cur_time = timer()
         time_taken = cur_time - import_start_time
         if time_taken >= 0.05:  # and not name.startswith("qualia") and not name.startswith("crypto"):
             print(
                 f'{time_taken:04.2f}s after {cur_time - start_time:04.2f}s (change: {(cur_time - last_time) * 1000:04.2f}ms) used to load module {name} on {current_thread().name}')
         last_time = cur_time
-        return result  # type: ignore[misc]
+        return result  # type: ignore
 
-    builtins.__import__ = custom_import  # type: ignore[assignment]
+    setattr(builtins, '__import__', custom_import)
